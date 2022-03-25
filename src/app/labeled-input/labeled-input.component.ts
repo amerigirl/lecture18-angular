aee42e5b7a8fa51636f5a110c4c0220f7dcb0c9b
Be-Asia <93811176+amerigirl@users.noreply.github.com>
2022-03-25 12:11:57 -0500

    finally got through the WHOLE thing!

---
+++ src/app/labeled-input/labeled-input.component.ts
@@ -0,0 +1,17 @@
+import { Component, ElementRef, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-labeled-input',
+  templateUrl: './labeled-input.component.html',
+  styleUrls: ['./labeled-input.component.css']
+})
+export class LabeledInputComponent implements OnInit {
+myTagName: string = '';
+  constructor(elem: ElementRef) {
+this.myTagName = elem.nativeElement.getAttribute('data-label')
+   }
+
+  ngOnInit() {
+  }
+
+}
